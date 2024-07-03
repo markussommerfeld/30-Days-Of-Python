2b353ed2af895f8271ab04a3d4c738756d9514bb
Markus Sommerfeld <markus_sommerfeld@yahoo.de>
2024-07-03 15:55:22 +0900

    Update variables by adding code from script on Github

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -38,3 +38,36 @@ print('Last name: ', last_name)
 print('Country: ', country)
 print('Age: ', age)
 print('Married: ', is_married)
+
+
+#------------------------------------------------------------------------------
+# # Try out input function
+# # doesn't work with Spyder < 5.3.0
+
+# first_name = input('What is your name: ')
+# age = input('How old are you? ')
+
+# print(first_name)
+# print(age)
+#------------------------------------------------------------------------------
+
+# Different python data types
+# Let's declare variables with various data types
+
+first_name = 'Asabeneh'     # str
+last_name = 'Yetayeh'       # str
+country = 'Finland'         # str
+city= 'Helsinki'            # str
+age = 250                   # int, it is not my real age, don't worry about it
+
+# Printing out types
+print(type('Asabeneh'))     # str
+print(type(first_name))     # str
+print(type(10))             # int
+print(type(3.14))           # float
+print(type(1 + 1j))         # complex
+print(type(True))           # bool
+print(type([1, 2, 3, 4]))     # list
+print(type({'name':'Asabeneh','age':250, 'is_married':250}))    # dict
+print(type((1,2)))                                              # tuple
+print(type(zip([1,2],[3,4])))                                   # set
