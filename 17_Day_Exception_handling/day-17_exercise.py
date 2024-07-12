6f6a3e45baa6935203ccaf5ab6fc10b1cb3a3b66
Markus Sommerfeld <markus_sommerfeld@yahoo.de>
2024-07-12 22:47:25 +0900

    17 was super short. Should have more tasks here

---
+++ 17_Day_Exception_handling/day-17_exercise.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 22:37:07 2024
+
+@author: marku
+"""
+"""
+Unpack the first five countries and store them in a variable nordic_countries,
+ store Estonia and Russia in es, and ru respectively.
+
+"""
+
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries, es, ru = names
